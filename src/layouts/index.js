f15b9f3d8f127f439dbd8381fe0eabe73c8ef0e8
Ernoff <ernest.offiong@gmail.com>
2017-10-03 16:31:34 +0100

    Sample code revamped to React with minimal footprint

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Navigation from './../components/Navigation'
 
-import '../assets/App.css'
-import logo from '../assets/logo.svg'
+import '../assets/index.css'
 
 const Layout = ({ children }) => (
   <div>
     <Navigation />
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
       </div>
       <div className="App-intro">{children}</div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
-import Layout from '../layouts'
+//import Layout from '../layouts'
 
 import Home from './Home'
-import About from './About/routes'
 
 export default {
   path: '/',
-  component: Layout,
-  indexRoute: Home,
-  childRoutes: [About]
+  component: Home,
+  indexRoute: Home
 }

978878bbfe9d0cdfd5fa1b0cbc135c8294c3983e
Junior Oliveira <arojunior@gmail.com>
2017-10-14 15:21:06 -0300

    class components converted to functions

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
-import Header from '../../components/Header'
+import React from 'react'
+import Header from 'components/Header'
 import LeftSide from './components/LeftSide'
 import MiddleBody from './components/MiddleBody'
 import RightSide from './components/RightSide'
-import '../../assets/App.css'
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="container">
-          <div className="row">
-            <LeftSide />
-            <MiddleBody />
-            <RightSide />
-          </div>
-        </div>
+import 'assets/App.css'
+
+const Home = () => (
+  <div>
+    <Header />
+    <div className="container">
+      <div className="row">
+        <LeftSide />
+        <MiddleBody />
+        <RightSide />
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
+
+export default Home

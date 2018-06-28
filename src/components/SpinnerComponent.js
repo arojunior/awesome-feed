77bbe9be2580415397966fe940d64367bcaf25ed
Junior Oliveira <arojunior@gmail.com>
2018-06-28 00:05:33 -0300

    Updates

--- src/components/SpinnerComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Loader from 'assets/img/loader.gif';
-
-export default () => (
-  <div>
-    <img src={Loader} alt="Loading...." />
-    {` `}
-  </div>
-);
--- src/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const API = `https://api.github.com`;
-export const GITHUB = `https://github.com`;
-export const GITHUB_GRAPHQL = `https://api.github.com/graphql`;

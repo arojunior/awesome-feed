d6bf8bcf2918e188faaaa4988feb702f0ae6521e
Weliton Resende <welitonderesende@gmail.com>
2018-12-28 05:06:22 -0200

    feat(navbar) improve mobile compatibility to navbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,26 +3,10 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
 const Header = ({ handleLogout }) => (
-  <div className="navbar navbar-default navbar-fixed-top">
+  <div className="navbar navbar-default navbar-fixed-top af-navbar">
     <div className="container-fluid">
-      <div className="navbar-header">
-        <button
-          type="button"
-          className="pull-left navbar-toggle collapsed"
-          data-toggle="collapse"
-          data-target="#navbar"
-          aria-expanded="false"
-          style={{ marginLeft: 10 }}
-        >
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-      </div>
-
-      <div id="navbar" className="navbar-collapse collapse" aria-expanded="true">
-        <ul className="nav navbar-nav">
+      <div id="navbar" className="navbar af-navbar-mobile" aria-expanded="true">
+        <ul className="nav navbar-nav af-navbar--nav">
           <li className="active">
             <a href="/">
               <span className="glyphicon glyphicon-home" /> Home

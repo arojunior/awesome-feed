9a6d79756f6c3c608cd5148da47b02de6f6864a3
Junior Oliveira <arojunior@gmail.com>
2019-06-29 17:22:55 -0300

    Updates

---
+++ precache-manifest.9e5ed20d03df667e29b7718da0fb7c3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb81c9d4087052a21f19c1bf829fd94d",
+    "url": "/static/media/GitHub-Logo-Web-Development-Community.bb81c9d4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7806974365798338ab5",
+    "url": "/static/js/main.b7806974.chunk.js"
+  },
+  {
+    "revision": "48f840bc020cba36188b",
+    "url": "/static/js/1.48f840bc.chunk.js"
+  },
+  {
+    "revision": "b7806974365798338ab5",
+    "url": "/static/css/main.f782efce.chunk.css"
+  },
+  {
+    "revision": "a2da7b34d0d550b8b3f699f2d2edbda3",
+    "url": "/index.html"
+  }
+];
